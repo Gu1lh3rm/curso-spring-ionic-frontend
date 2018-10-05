4ca384c2330befe72523756f4515cb5eb8b9baec
lourenco.guilherme <guilherme@ampliato.com.br>
2018-10-05 12:00:48 -0300

    Configuracao basica para fazer login com firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
 import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../providers/auth/auth-service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZFHkdEuyiOV_TIwaZKXCKfFZroZ1rIPo",
+  authDomain: "gml-curso-spring-ionic-front.firebaseapp.com",
+  databaseURL: "https://gml-curso-spring-ionic-front.firebaseio.com",
+  projectId: "gml-curso-spring-ionic-front",
+  storageBucket: "gml-curso-spring-ionic-front.appspot.com",
+  messagingSenderId: "626264654919"
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}

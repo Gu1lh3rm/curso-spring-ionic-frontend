572c08158025fc452f75d374023c7875017a2a25
lourenco.guilherme <guilherme@ampliato.com.br>
2018-10-10 11:29:27 -0300

    Revert "Atualização da classe profile com imagem de avatar"
    This reverts commit a85e809b0cd95f5f83ac6eb8d7fcc821e32d3f00.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageProvider } from '../../providers/storage/storage';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteProvider } from '../../providers/cliente/cliente';
-import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -11,7 +10,7 @@ import { API_CONFIG } from '../../config/api.config';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
   cliente: ClienteDTO;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageProvider, public clienteProvider: ClienteProvider) {
@@ -22,12 +21,8 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteProvider.findByEmail(localUser.email)
       .subscribe(response => {
-        if(response.imgUrl){
-          response.imgUrl = this.bucketUrl + response.imgUrl;
-        } else{
-          response.imgUrl = 'assets/imgs/avatar-blank.png'
-        }
         this.cliente = response;
+        //buscar imagem
       }, error => {});
     }
   }
